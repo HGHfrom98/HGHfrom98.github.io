eb2e42f81f7d77d86ab3561e4b611ff22a082053
Hunter <122322729+HGHfrom98@users.noreply.github.com>
2024-02-11 13:01:55 -0500

    Add files via upload

---
+++ Showcase.js
@@ -0,0 +1,167 @@
+//Hide sections 2-10, leave section 1
+document.getElementById("section2").style.display = "none";
+document.getElementById("section3").style.display = "none";
+document.getElementById("section4").style.display = "none";
+document.getElementById("section5").style.display = "none";
+document.getElementById("section6").style.display = "none";
+document.getElementById("section7").style.display = "none";
+document.getElementById("section8").style.display = "none";
+document.getElementById("section9").style.display = "none";
+document.getElementById("section10").style.display = "none";
+
+function section1() {
+    //hide all but section1
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+    document.getElementById("section4").style.display = "none";
+    document.getElementById("section5").style.display = "none";
+    document.getElementById("section6").style.display = "none";
+    document.getElementById("section7").style.display = "none";
+    document.getElementById("section8").style.display = "none";
+    document.getElementById("section9").style.display = "none";
+    document.getElementById("section10").style.display = "none";
+    //display section1
+    document.getElementById("section1").style.display = "block";
+}
+
+function section2() {
+    //hide all but section2
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+    document.getElementById("section4").style.display = "none";
+    document.getElementById("section5").style.display = "none";
+    document.getElementById("section6").style.display = "none";
+    document.getElementById("section7").style.display = "none";
+    document.getElementById("section8").style.display = "none";
+    document.getElementById("section9").style.display = "none";
+    document.getElementById("section10").style.display = "none";
+    //display section2
+    document.getElementById("section2").style.display = "block";
+    
+}
+
+
+function section3() {
+    //hide all but section3
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section4").style.display = "none";
+    document.getElementById("section5").style.display = "none";
+    document.getElementById("section6").style.display = "none";
+    document.getElementById("section7").style.display = "none";
+    document.getElementById("section8").style.display = "none";
+    document.getElementById("section9").style.display = "none";
+    document.getElementById("section10").style.display = "none";
+    //display section3
+    document.getElementById("section3").style.display = "block";
+}
+
+function section4() {
+    //hide all but section4
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+    document.getElementById("section5").style.display = "none";
+    document.getElementById("section6").style.display = "none";
+    document.getElementById("section7").style.display = "none";
+    document.getElementById("section8").style.display = "none";
+    document.getElementById("section9").style.display = "none";
+    document.getElementById("section10").style.display = "none";
+    //display section4
+    document.getElementById("section4").style.display = "block";
+}
+
+function section5() {
+    //hide all but section5
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+    document.getElementById("section4").style.display = "none";
+    document.getElementById("section6").style.display = "none";
+    document.getElementById("section7").style.display = "none";
+    document.getElementById("section8").style.display = "none";
+    document.getElementById("section9").style.display = "none";
+    document.getElementById("section10").style.display = "none";
+    //display section5
+    document.getElementById("section5").style.display = "block";
+}
+
+
+function section6() {
+    //hide all but section6
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+    document.getElementById("section4").style.display = "none";
+    document.getElementById("section5").style.display = "none";
+    document.getElementById("section7").style.display = "none";
+    document.getElementById("section8").style.display = "none";
+    document.getElementById("section9").style.display = "none";
+    document.getElementById("section10").style.display = "none";
+    //display section6
+    document.getElementById("section6").style.display = "block";  
+}
+
+function section7() {
+    //hide all but section7
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+    document.getElementById("section4").style.display = "none";
+    document.getElementById("section5").style.display = "none";
+    document.getElementById("section6").style.display = "none"; 
+    document.getElementById("section8").style.display = "none";
+    document.getElementById("section9").style.display = "none";
+    document.getElementById("section10").style.display = "none";
+    //display section7
+    document.getElementById("section7").style.display = "block";  
+}
+
+function section8() {
+    //hide all but section8
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+    document.getElementById("section4").style.display = "none";
+    document.getElementById("section5").style.display = "none";
+    document.getElementById("section6").style.display = "none"; 
+    document.getElementById("section7").style.display = "none";
+    document.getElementById("section9").style.display = "none";
+    document.getElementById("section10").style.display = "none";
+    //display section8
+    document.getElementById("section8").style.display = "block";  
+}
+
+function section9() {
+    //hide all but section9
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+    document.getElementById("section4").style.display = "none";
+    document.getElementById("section5").style.display = "none";
+    document.getElementById("section6").style.display = "none"; 
+    document.getElementById("section7").style.display = "none";
+    document.getElementById("section8").style.display = "none";
+    document.getElementById("section10").style.display = "none";
+    //display section9
+    document.getElementById("section9").style.display = "block";    
+}
+
+function section10() {
+    //hide all but section10
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+    document.getElementById("section4").style.display = "none";
+    document.getElementById("section5").style.display = "none";
+    document.getElementById("section6").style.display = "none"; 
+    document.getElementById("section7").style.display = "none";
+    document.getElementById("section8").style.display = "none";
+    document.getElementById("section9").style.display = "none";
+    //display section10
+    document.getElementById("section10").style.display = "block";    
+}
+
+function section11() {
+    alert("This section will be dedicated to my virtual network. \n I am curently adding virtual workstations to my domain. \n Content will be added soon, though. Please check back in!");
+}
